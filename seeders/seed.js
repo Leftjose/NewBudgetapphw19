4ddd69c686ad89293cedfc588d85aa2eb8a4b93b
Jose Izquierdo <leftjose@gmail.com>
2021-12-11 23:01:16 -0600

    adding files

---
+++ seeders/seed.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+var db = require("../models");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/imageperformance",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+var imageSeed = [
+    image; ""
+]
